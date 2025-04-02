b7e4d865bd8e70a26b9e026f46d34adfcfd5fcc6
Tim Deres <50623315+timderes@users.noreply.github.com>
2025-04-02 12:00:32 +0200

    Add drawer for meta data

--- renderer/pages/[locale]/create/article.tsx
+++ renderer/pages/[locale]/create/article.tsx
@@ -1,22 +1,21 @@
 import { useTranslation } from "next-i18next";
 import { getStaticPaths, makeStaticProperties } from "@/lib/getStatic";
 
-import { Container } from "@mantine/core";
-import { RichTextEditor, Link } from '@mantine/tiptap';
+import { Button, Drawer, ScrollArea } from "@mantine/core";
+import { RichTextEditor, Link } from "@mantine/tiptap";
 
-import DefaultLayout from "@/components/layouts/Default";
-import PageHeader from "@/components/content/PageHeader";
+import DefaultLayout, { headerHeight } from "@/components/layouts/Default";
 
-import { useEditor } from "@tiptap/react";
-import Highlight from '@tiptap/extension-highlight';
-import StarterKit from '@tiptap/starter-kit';
-import Underline from '@tiptap/extension-underline';
-import TextAlign from '@tiptap/extension-text-align';
-import Superscript from '@tiptap/extension-superscript';
-import SubScript from '@tiptap/extension-subscript';
+import { useEditor, BubbleMenu } from "@tiptap/react";
+import Highlight from "@tiptap/extension-highlight";
+import StarterKit from "@tiptap/starter-kit";
+import Underline from "@tiptap/extension-underline";
+import TextAlign from "@tiptap/extension-text-align";
+import Superscript from "@tiptap/extension-superscript";
+import SubScript from "@tiptap/extension-subscript";
+import { useDisclosure } from "@mantine/hooks";
 
-
-const IndexPage = () => {
+const CreateArticlePage = () => {
     const { t } = useTranslation();
     const editor = useEditor({
         extensions: [
@@ -28,74 +27,102 @@ const IndexPage = () => {
             Highlight,
             TextAlign.configure({
                 types: ["heading", "paragraph"],
-            })
+            }),
         ],
         immediatelyRender: false,
-        content: "Schreiben Sie ihr erstes Dokument hier...",
-    })
-
-    console.info(editor?.getJSON())
+        content:
+            "<h1>Neuer Artikel</h1> <p>Beginnen Sie, indem Sie diesen Text oder die Überschrift bearbeiten...</p>",
+    });
+    const [opened, { open, close }] = useDisclosure(false);
 
     return (
-        <DefaultLayout withNavbarOpen>
-            <Container fluid>
-                <PageHeader title={t("routes.create_article")}>
-                    ...
-                </PageHeader>
-                <RichTextEditor editor={editor} variant="subtle">
-                    <RichTextEditor.Toolbar sticky stickyOffset={60}>
+        <DefaultLayout withNavbarOpen={false}>
+            <Drawer
+                opened={opened}
+                scrollAreaComponent={ScrollArea.Autosize}
+                onClose={close}
+                title={t("editor:metaDrawerTitle")}
+                position="right"
+                overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+            >
+                {/* Drawer content */}
+            </Drawer>
+            <RichTextEditor
+                editor={editor}
+                variant="subtle"
+                style={{
+                    borderRadius: 0,
+                }}
+            >
+                {editor && (
+                    <BubbleMenu editor={editor}>
                         <RichTextEditor.ControlsGroup>
                             <RichTextEditor.Bold />
                             <RichTextEditor.Italic />
                             <RichTextEditor.Underline />
                             <RichTextEditor.Strikethrough />
                             <RichTextEditor.ClearFormatting />
-                            <RichTextEditor.Highlight />
-                            <RichTextEditor.Code />
                         </RichTextEditor.ControlsGroup>
+                    </BubbleMenu>
+                )}
+                <RichTextEditor.Toolbar sticky stickyOffset={headerHeight}>
+                    <RichTextEditor.ControlsGroup
+                        display="block"
+                        style={{ width: "100%" }}
+                    >
+                        <Button onClick={open}>{t("editor:showMetaDrawer")}</Button>
+                    </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Bold />
+                        <RichTextEditor.Italic />
+                        <RichTextEditor.Underline />
+                        <RichTextEditor.Strikethrough />
+                        <RichTextEditor.ClearFormatting />
+                        <RichTextEditor.Highlight />
+                        <RichTextEditor.Code />
+                    </RichTextEditor.ControlsGroup>
 
-                        <RichTextEditor.ControlsGroup>
-                            <RichTextEditor.H1 />
-                            <RichTextEditor.H2 />
-                            <RichTextEditor.H3 />
-                            <RichTextEditor.H4 />
-                        </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.H1 />
+                        <RichTextEditor.H2 />
+                        <RichTextEditor.H3 />
+                        <RichTextEditor.H4 />
+                    </RichTextEditor.ControlsGroup>
 
-                        <RichTextEditor.ControlsGroup>
-                            <RichTextEditor.Blockquote />
-                            <RichTextEditor.Hr />
-                            <RichTextEditor.BulletList />
-                            <RichTextEditor.OrderedList />
-                            <RichTextEditor.Subscript />
-                            <RichTextEditor.Superscript />
-                        </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Blockquote />
+                        <RichTextEditor.Hr />
+                        <RichTextEditor.BulletList />
+                        <RichTextEditor.OrderedList />
+                        <RichTextEditor.Subscript />
+                        <RichTextEditor.Superscript />
+                    </RichTextEditor.ControlsGroup>
 
-                        <RichTextEditor.ControlsGroup>
-                            <RichTextEditor.Link />
-                            <RichTextEditor.Unlink />
-                        </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Link />
+                        <RichTextEditor.Unlink />
+                    </RichTextEditor.ControlsGroup>
 
-                        <RichTextEditor.ControlsGroup>
-                            <RichTextEditor.AlignLeft />
-                            <RichTextEditor.AlignCenter />
-                            <RichTextEditor.AlignJustify />
-                            <RichTextEditor.AlignRight />
-                        </RichTextEditor.ControlsGroup>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.AlignLeft />
+                        <RichTextEditor.AlignCenter />
+                        <RichTextEditor.AlignJustify />
+                        <RichTextEditor.AlignRight />
+                    </RichTextEditor.ControlsGroup>
 
-                        <RichTextEditor.ControlsGroup>
-                            <RichTextEditor.Undo />
-                            <RichTextEditor.Redo />
-                        </RichTextEditor.ControlsGroup>
-                    </RichTextEditor.Toolbar>
+                    <RichTextEditor.ControlsGroup>
+                        <RichTextEditor.Undo />
+                        <RichTextEditor.Redo />
+                    </RichTextEditor.ControlsGroup>
+                </RichTextEditor.Toolbar>
 
-                    <RichTextEditor.Content />
-                </RichTextEditor>
-            </Container>
+                <RichTextEditor.Content />
+            </RichTextEditor>
         </DefaultLayout>
     );
 };
 
-export default IndexPage;
+export default CreateArticlePage;
 
 export const getStaticProps = makeStaticProperties(["common"]);
 
