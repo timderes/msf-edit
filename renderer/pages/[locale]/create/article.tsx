2ba86eb8d0865c0b0b09c4b8cae64e7e8ab5a914
Tim Deres <50623315+timderes@users.noreply.github.com>
2025-04-02 11:39:50 +0200

    Add editor

--- renderer/pages/[locale]/create/article.tsx
+++ renderer/pages/[locale]/create/article.tsx
@@ -2,12 +2,39 @@ import { useTranslation } from "next-i18next";
 import { getStaticPaths, makeStaticProperties } from "@/lib/getStatic";
 
 import { Container } from "@mantine/core";
+import { RichTextEditor, Link } from '@mantine/tiptap';
 
 import DefaultLayout from "@/components/layouts/Default";
 import PageHeader from "@/components/content/PageHeader";
 
+import { useEditor } from "@tiptap/react";
+import Highlight from '@tiptap/extension-highlight';
+import StarterKit from '@tiptap/starter-kit';
+import Underline from '@tiptap/extension-underline';
+import TextAlign from '@tiptap/extension-text-align';
+import Superscript from '@tiptap/extension-superscript';
+import SubScript from '@tiptap/extension-subscript';
+
+
 const IndexPage = () => {
     const { t } = useTranslation();
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Underline,
+            Link,
+            Superscript,
+            SubScript,
+            Highlight,
+            TextAlign.configure({
+                types: ["heading", "paragraph"],
+            })
+        ],
+        immediatelyRender: false,
+        content: "Schreiben Sie ihr erstes Dokument hier...",
+    })
+
+    console.info(editor?.getJSON())
 
     return (
         <DefaultLayout withNavbarOpen>
@@ -15,6 +42,54 @@ const IndexPage = () => {
                 <PageHeader title={t("routes.create_article")}>
                     ...
                 </PageHeader>
+                <RichTextEditor editor={editor} variant="subtle">
+                    <RichTextEditor.Toolbar sticky stickyOffset={60}>
+                        <RichTextEditor.ControlsGroup>
+                            <RichTextEditor.Bold />
+                            <RichTextEditor.Italic />
+                            <RichTextEditor.Underline />
+                            <RichTextEditor.Strikethrough />
+                            <RichTextEditor.ClearFormatting />
+                            <RichTextEditor.Highlight />
+                            <RichTextEditor.Code />
+                        </RichTextEditor.ControlsGroup>
+
+                        <RichTextEditor.ControlsGroup>
+                            <RichTextEditor.H1 />
+                            <RichTextEditor.H2 />
+                            <RichTextEditor.H3 />
+                            <RichTextEditor.H4 />
+                        </RichTextEditor.ControlsGroup>
+
+                        <RichTextEditor.ControlsGroup>
+                            <RichTextEditor.Blockquote />
+                            <RichTextEditor.Hr />
+                            <RichTextEditor.BulletList />
+                            <RichTextEditor.OrderedList />
+                            <RichTextEditor.Subscript />
+                            <RichTextEditor.Superscript />
+                        </RichTextEditor.ControlsGroup>
+
+                        <RichTextEditor.ControlsGroup>
+                            <RichTextEditor.Link />
+                            <RichTextEditor.Unlink />
+                        </RichTextEditor.ControlsGroup>
+
+                        <RichTextEditor.ControlsGroup>
+                            <RichTextEditor.AlignLeft />
+                            <RichTextEditor.AlignCenter />
+                            <RichTextEditor.AlignJustify />
+                            <RichTextEditor.AlignRight />
+                        </RichTextEditor.ControlsGroup>
+
+                        <RichTextEditor.ControlsGroup>
+                            <RichTextEditor.Undo />
+                            <RichTextEditor.Redo />
+                        </RichTextEditor.ControlsGroup>
+                    </RichTextEditor.Toolbar>
+
+                    <RichTextEditor.Content />
+                </RichTextEditor>
             </Container>
         </DefaultLayout>
     );
--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -9,6 +9,7 @@ import "@mantine/core/styles.css";
 import "@mantine/dropzone/styles.css";
 import "@mantine/notifications/styles.css";
 import "@mantine/charts/styles.css";
+import '@mantine/tiptap/styles.css';
 
 // Put overrides with custom stylesheets here
 import "../styles/globals.css";
